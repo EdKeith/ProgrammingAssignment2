5512299efce2c1e533c9015f82e1dd1e341d0bc1
Ed Keith <ed_keith@acm.org>
2014-12-17 12:46:16 -0500

    implimentation

--- tests/1.R
+++ tests/1.R
@@ -1,8 +1,15 @@
 ## Unit tests for Programming Assignment 1
 
-test.examples <- function()
+test.createandget <- function()
 {
-  c = rbind(c(1, -1/4), c(-1/4, 1))
-  ident = rbind(c(1,0),c(0,1))
-  checkEquals(c %*% solve(c), ident)
+  m <- matrix(c(1,2,3,4), 2,2)
+  c <- makeCacheMatrix(m)
+  checkEquals(c$get(), m)
+}
+
+test.inverse <- function()
+{
+  c <- makeCacheMatrix(rbind(c(1, -1/4), c(-1/4, 1)))
+  ident <- rbind(c(1,0),c(0,1))
+  checkEquals(c$get() %*% cacheSolve(c), ident)
 }
